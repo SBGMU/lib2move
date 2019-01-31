261cd03f4786fa62e84fea7d8a0bc268040e457c
kapserge <kapserge@gmail.com>
2019-01-31 16:02:46 +0100

    final user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,77 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\LoginUserType;
+use App\Form\RegisterUserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class UserController extends AbstractController
+class UserController extends Controller
 {
     /**
      * @Route("/user", name="user")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request, UserRepository $userRepository)
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $user = new User();
+        $form = $this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        $users = $userRepository->findAll();
+
+        return $this->render('security/admin.html.twig', array(
+
+                'form'=>$form->createView(),
+                
+                'users'=>$users
+            )
+
+        );
     }
+
+      /**
+     * @Route("/register/admin", name="register_admin")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder,AuthenticationUtils $authenticationUtils)
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $user->setRoles(["ROLES_ADMIN"]);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            //$logger->info('User registered now !');
+            //$this->addFlash('notice', 'Your changes were saved!');
+            //$event = new UserRegisteredEvent($user);
+            //$eventDispatcher->dispatch(UserRegisteredEvent::NAME,$event);
+
+            //return $this->redirectToRoute('video');
+        }
+        return $this->render('user/RegisterAdmin.html.twig', [
+            'controller_name' => 'SecurityController',
+            'error' => $authenticationUtils ->getLastAuthenticationError(),
+            'form' => $form->createView()
+        ]);
+            
+    }
+     
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,12 +5,25 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\User;
+use App\Form\ProfileUserType;
+use App\Form\UserType;
+//use App\Entity\Video;
+//use App\Form\VideoType;
+//use App\Repository\VideoRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
-{
+class User implements UserInterface
+ {
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -58,9 +71,15 @@ class User
      */
     private $locations;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
     public function __construct()
     {
         $this->locations = new ArrayCollection();
+        $this->roles = ["ROLE_USER"];
     }
 
     public function getId(): ?int
@@ -104,54 +123,109 @@ class User
         return $this;
     }
 
-    public function getRoles(): ?array
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
     {
         return $this->roles;
     }
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
     {
         return $this->password;
     }
 
-    public function setPassword(string $password): self
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
     {
-        $this->password = $password;
-
-        return $this;
+        return null;
     }
 
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param mixed $roles
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+        return $this;
+    }
     public function getTelephone(): ?string
     {
         return $this->Telephone;
     }
-
     public function setTelephone(string $Telephone): self
     {
         $this->Telephone = $Telephone;
-
         return $this;
     }
-
     public function getAdresse(): ?string
     {
         return $this->Adresse;
     }
-
     public function setAdresse(string $Adresse): self
     {
         $this->Adresse = $Adresse;
-
         return $this;
     }
 
+
+    
     /**
      * @return Collection|Location[]
      */
@@ -179,6 +253,18 @@ class User
                 $location->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
 
         return $this;
     }
