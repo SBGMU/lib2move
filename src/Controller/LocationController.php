c79a6fbf37aea52d85d6df05051ee2c547adb707
root <kapserge@gmail.com>
2019-03-11 17:34:00 +0100

    mise a jour location

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -8,6 +8,7 @@ use App\Entity\Location;
 use App\Form\VehiculeType;
 use App\Form\LocationType;
 use App\Repository\VehiculeRepository;
+use App\Repository\LocationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,9 +21,7 @@ use Symfony\Component\Validator\Constraints\DateTime;
 
 class LocationController extends AbstractController
 {
-    /**
-     * @Route("/location", name="location")
-     */
+    
     /**
      * @Route("/location/{id}", name="location")
      * @ParamConverter("location", options={"mapping"={"id"="id"}})
@@ -36,11 +35,6 @@ class LocationController extends AbstractController
         $user =  new User();
         $form = $this->createForm(LocationType:: class, $location);
         $vehicule = $this->getDoctrine()->getRepository(Vehicule::class)->find($id);  
-        // Configure Dompdf according to your needs
-        $pdfOptions = new Options();
-        $pdfOptions->set('defaultFont', 'Arial');
-        // Retrieve the HTML generated in our twig file
-
         
         $form->handleRequest($request);
             if (!$vehicule) {
@@ -71,13 +65,61 @@ $location->setIdVehicule($vehicule);
                 $entityManager->persist($location);
                 $entityManager->flush();
                 
+               // return $this->redirectToRoute('dashboard');
             }
         
             
-        return $this->render('location/index.html.twig', [
-
-            'vehicule' => $vehicule, 'form' => $form->createView()
-    
-        ]);
+        return $this->render('location/index.html.twig', ['vehicule' => $vehicule, 'form' => $form->createView()]);
     }
+
+    /**
+     * @Route("/paiementFait/{id}", name="paiementFait")
+     * @ParamConverter("paiementFait", options={"mapping"={"id"="id"}})
+     */
+    public function paiementFait($id,Request $request, LocationRepository $locationRepository)
+    {
+        $location = $this->getDoctrine()->getRepository(Location::class)->find($id); 
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('vehicule/paiementEffectue.html.twig', array(
+            'location' => $location));
+        
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render('vehicule/paiementEffectue.html.twig', array(
+            'location' => $location,
+        ));
+
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("vehicule/paiementEffectue.html.twig", [
+            "Attachment" => false
+        ]);
+        // return $this->render('vehicule/paiementEffectue.html.twig', array(
+        //     'vehicule' => 'vehicule',
+        // ));
+
+    } 
+    
+    
+    /**
+     * @Route("/paiementEffectue/{id}", name="paiementEffectue")
+     * @ParamConverter("paiementEffectue", options={"mapping"={"id"="id"}})
+     */
+    public function paiementEffectue($id, Request $request, VehiculeRepository $vehiculeRepository)
+    {
+        $location = $this->getDoctrine()->getRepository(Location::class)->find($id);
+        return $this->render('vehicule/paiementEffectue.html.twig', array(
+            'location' => $location,
+        ));
+
+    } 
 }
